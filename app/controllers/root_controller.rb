39aca5f061763b040cccb06ca84d7f91d5039044
Aminu Rabiu <arabiu033@gmail.com>
2023-02-22 14:12:39 +0100

    Fix the rendering

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,11 +1,6 @@
 class RootController < ApplicationController
   def index
     @greeting = helpers.random_greetings
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml { render xml: @greeting }
-      format.json { render json: @greeting }
-    end
+    render json: @greeting
   end
 end
